876aefae94f68862346cd2609206dd11863f2cb2
JulianOrinyol <julianorinyol@gmail.com>
2015-11-05 21:30:29 -0800

    created Post and Comment models and migrations

--- Gemfile
+++ Gemfile
@@ -13,7 +13,13 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+
+
+# Downgraded this from the newer version because of a bug with 'angular-rails-templates'
 gem 'sprockets', '2.12.3'
+
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151106052818) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "upvotes"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "link"
+    t.integer  "upvotes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
