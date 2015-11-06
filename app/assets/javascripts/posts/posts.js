6d6817919db39faa37c93518bdd09fdabca96cd6
JulianOrinyol <julianorinyol@gmail.com>
2015-11-06 12:34:50 -0800

    angular posts service can now retrieve or create posts in the backend

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -3,20 +3,19 @@ angular.module('vancouverNews')
   '$http', 
   function($http){
     var o = {
-        posts: [
-          {title: 'title 1', upvotes: 5, comments: []},
-          {title: 'title 2', upvotes: 2, comments: []},
-          {title: 'title 3', upvotes: 15, comments: []},
-          {title: 'title 4', upvotes: 9, comments: []},
-          {title: 'title 5', upvotes: 4, comments: []}
-        ]
+        posts: []
       };
     
     o.getAll = function() {
-        return $http.get('/posts.json').success(function(data){
+        return $http.get('/posts.json')
+        .success(function(data) {
           angular.copy(data, o.posts);
         });
       };
-
+    o.create = function(post) {
+      return $http.post('/posts.json', post).success(function(data){
+        o.posts.push(data);
+      });
+    };
     return o;
 }])
