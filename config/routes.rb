9274e9a371dcd4f40562b0f3e2cf3fc229452c95
JulianOrinyol <julianorinyol@gmail.com>
2015-11-06 17:09:51 -0800

    added devise login ability from angular

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'page#angular'
+  root 'application#angular'
 
   resources :posts, only: [:create, :index, :show] do
       resources :comments, only: [:show, :create] do
